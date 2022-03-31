be0d162f38ecacdad0dc5d8d79e2d9f39a8ac92f
Leonard <leonard.peltea@yahoo.com>
2022-03-31 18:52:14 +0100

    commit on master
    Signed-off-by: Leonard <leonard.peltea@yahoo.com>

--- static/js/game.js
+++ static/js/game.js
@@ -4,13 +4,13 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarrFull = document.querySelector('#progressBarrFull');
 
-let currentQuestion ={}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion ={};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
-let questions = [
+const questions = [
     {
         question: 'Which is the capital of Germany?',
             choice1: 'Berlin', 
@@ -175,67 +175,66 @@ let questions = [
     }
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 20
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 20;
 
-startGame =() => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
+const startGame =() => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
 
-getNewQuestion = () => {
+const getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-
-        return window.location.assign('/end.html')
-    } 
+        localStorage.setItem('mostRecentScore', score);
+        return window.location.assign('./end.html');
+    };
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
     progressBarrFull.style.width = `${(questionCounter/MAX_QUESTIONS) *100}%`
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
-    })
+    });
 
-    availableQuestions.splice(questionsIndex, 1)
+    availableQuestions.splice(questionsIndex, 1);
 
-    acceptingAnswers = true
-}
+    acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
     choice.addEventListener('click', e =>{
         if(!acceptingAnswers) return
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
         if(classToApply === 'correct'){
             incrementScore(SCORE_POINTS)
-        }
+        };
 
         selectedChoice.parentElement.classList.add(classToApply)
 
         setTimeout(() =>{
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-        }, 1000)
-    })
-})
+        }, 1000);
+    });
+});
 
 incrementScore = num => {
     score +=num
     scoreText.innerText = score
-}
+};
 
-startGame()
+startGame();
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,26 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+const openModal = (modal) => {
+    if (modal==null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+};
+
+const closeModal = (modal) => {
+    if (modal==null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+};
+
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () =>{
         const modal = document.querySelector(button.dataset.modalTarget)
         openModal(modal)
     })
-})
+});
 
 overlay .addEventListener('click', () =>{
     const modals = document.querySelectorAll('.modal.active')
@@ -15,23 +28,12 @@ overlay .addEventListener('click', () =>{
         closeModal(modal)
 
     })
-})
+});
 
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () =>{
         const modal = button.closest('.modal')
         closeModal(modal)
     })
-})
+});
 
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
